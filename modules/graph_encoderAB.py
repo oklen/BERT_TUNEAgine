a34b3c6fba7c3af5a67c45bbdfef2fdce0910e7d
oklens <3294005475@qq.com>
2020-08-26 07:42:19 +0800

    update

--- modules/graph_encoderAB.py
+++ modules/graph_encoderAB.py
@@ -581,9 +581,8 @@ class Encoder(nn.Module):
             
 #        x_all = x_all[:,1:,:]
 #        print(x_all.shape)
-        print(x_all.shape)
-        exit(0)
-        return torch.cat([torch.mean(x_all[:,-1,:],0),torch.mean(x_all[:,-2,:],0)],-1)
+ 
+        return torch.cat([torch.mean(x_all[:,-1,:].view(hidden_states.shape),1),torch.mean(x_all[:,-2,:].view(hidden_states.shape),1)],-1)
         x = x_all.mean(1)
         
 #        print(x.shape)
