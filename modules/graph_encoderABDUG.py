a2f48d7112d417faca2e3a2c86d398b317b38e88
oklens <3294005475@qq.com>
2020-08-26 09:01:59 +0800

    update

--- modules/graph_encoderABDUG.py
+++ modules/graph_encoderABDUG.py
@@ -580,8 +580,8 @@ class CollaborativeAttention(nn.Module):
         # (batch, 1, to_seq, dim)
         mixed_key = key_layer[..., None, :, :]
         
-        print(mixed_query.shape)
-        print(mixed_key.shape)
+#        print(mixed_query.shape)
+#        print(mixed_key.shape)
         # (batch, head, from_seq, to_seq)
         attention_scores = torch.matmul(mixed_query, mixed_key.transpose(-1, -2))
 
