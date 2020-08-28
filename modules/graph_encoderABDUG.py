d737657093eda15818118fe4d4856ac85ee9dbd0
oklens <3294005475@qq.com>
2020-08-28 20:32:33 +0800

    update

--- modules/graph_encoderABDUG.py
+++ modules/graph_encoderABDUG.py
@@ -713,7 +713,7 @@ class MultiHeadedAttention(nn.Module):
         # 1) Do all the linear projections in batch from d_model => h x d_k 
         query, key, value = \
             [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
-             for l, x in zip(self.linears, (query, key, value))]
+             for l in self.linears for x in (query, key, value)]
         
         # 2) Apply attention on all the projected vectors in batch. 
         x, self.attn = attention(query, key, value, mask=mask, 
