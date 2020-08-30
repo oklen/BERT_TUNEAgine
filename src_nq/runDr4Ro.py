15b1779f22f900c1a53c5018705a99ce0b0bf99f
oklens <3294005475@qq.com>
2020-08-30 16:20:00 +0800

    update

--- src_nq/runDr4Ro.py
+++ src_nq/runDr4Ro.py
@@ -456,7 +456,7 @@ def main():
 
                     if (step + 1) % args.gradient_accumulation_steps == 0:
                         optimizer.step()
-#                        scheduler.step()
+                        scheduler.step()
                         optimizer.zero_grad()
                         global_step += 1
 
