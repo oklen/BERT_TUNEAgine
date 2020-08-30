d225cf738628751c0a5ccbf57c9109a2a17d9b52
oklens <3294005475@qq.com>
2020-08-30 08:54:47 +0800

    update

--- src_nq/modelDR3.py
+++ src_nq/modelDR3.py
@@ -85,12 +85,12 @@ class NqModel(nn.Module):
                 graph_output = self.encoder(sequence_output, st_mask, (edges_src, edges_tgt, edges_type, edges_pos), output_all_encoded_layers=False)
             else:
     
-                input_ids = input_ids.to('cuda:0')
-                attention_mask = attention_mask.to('cuda:0')
-                token_type_ids = token_type_ids.to('cuda:0')
+#                input_ids = input_ids.to('cuda:0')
+#                attention_mask = attention_mask.to('cuda:0')
+#                token_type_ids = token_type_ids.to('cuda:0')
                 sequence_output = []
                 for i in range(input_ids.size(0)):
-                    sequence_tmp,_ = self.bert(input_ids[i].unsqueeze(0),  attention_mask[i].unsqueeze(0),token_type_ids[i].unsqueeze(0))
+                    sequence_tmp,_ = self.bert(input_ids[i].to('cuda:0').unsqueeze(0),  attention_mask[i].to('cuda:0').unsqueeze(0),token_type_ids[i].to('cuda:0').unsqueeze(0))
                     sequence_output.append(sequence_tmp)
                 #sequence_output2, _ = self.bert2(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
                 #print(type(sequence_output),sequence_output.shape)
