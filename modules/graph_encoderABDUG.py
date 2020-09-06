ef6e20c643a60d6732fe721179fa6ee5e223e6d4
oklens <3294005475@qq.com>
2020-09-06 17:07:19 +0800

    update

--- modules/graph_encoderABDUG.py
+++ modules/graph_encoderABDUG.py
@@ -4,7 +4,7 @@ import copy
 import json
 import torch
 import torch.nn as nn
-from torch_geometric.nn import GraphConv,AGNNConv,FastRGCNConv,RGCNConv,DNAConv
+#from torch_geometric.nn import GraphConv,AGNNConv,FastRGCNConv,RGCNConv,DNAConv
 
 
 class EdgeType(enum.IntEnum):
