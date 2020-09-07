40689fe8290fdb13e08b29d4f11780612d42411f
oklens <3294005475@qq.com>
2020-09-07 21:07:31 +0800

    update

--- src_nq/modelDR3.py
+++ src_nq/modelDR3.py
@@ -34,7 +34,7 @@ class NqModel(nn.Module):
         #self.bert =  RobertaModel(RobertaConfig(max_position_embeddings=514,vocab_size=50265))
 
         #print(my_config,bert_config)
-        self.tok_dense = nn.Linear(my_config.hidden_size*3, my_config.hidden_size*3)
+        self.tok_dense = nn.Linear(my_config.hidden_size*2, my_config.hidden_size*2)
         
         self.tok_dense2 = nn.Linear(my_config.hidden_size, my_config.hidden_size)
 #        self.para_dense = nn.Linear(self.config.hidden_size, self.config.hidden_size)
@@ -42,7 +42,7 @@ class NqModel(nn.Module):
         
         self.dropout = nn.Dropout(my_config.hidden_dropout_prob)
 
-        self.tok_outputs = nn.Linear(my_config.hidden_size*3, 1) # tune to avoid fell into bad places
+        self.tok_outputs = nn.Linear(my_config.hidden_size*2, 1) # tune to avoid fell into bad places
         self.tok_outputs2 = nn.Linear(my_config.hidden_size, 1)
 #        config.max_token_len, config.max_token_relative
 #        self.para_outputs = nn.Linear(self.config.hidden_size, 1)
@@ -125,7 +125,7 @@ class NqModel(nn.Module):
 #            print(graph_output[:,0])
 #            x = torch.cat((graph_output[:,0],sequence_output[:,0]),-1)
             x = graph_output
-            x = torch.cat((x,sequence_output[:,0]),-1)
+#            x = torch.cat((x,sequence_output[:,0]),-1)
 # DDDD
             x = self.dropout(x)
             tok_logits.append(self.tok_outputs(self.dropout(torch.tanh(self.tok_dense(x)))).squeeze(-1))
