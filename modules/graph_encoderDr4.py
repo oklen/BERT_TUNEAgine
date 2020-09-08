241e876ffabfd33ed278058165bc406a6bc29ea4
oklens <3294005475@qq.com>
2020-09-08 18:09:23 +0800

    update

--- modules/graph_encoderDr4.py
+++ modules/graph_encoderDr4.py
@@ -691,12 +691,12 @@ def attention(query, key, value, mask=None, dropout=None):
 
 
 class MultiHeadedAttention(nn.Module):
-    def __init__(self, h, d_model, dropout=0.1):
+    def __init__(self, h, d_model, dropout=0.15):
         "Take in model size and number of heads."
         super(MultiHeadedAttention, self).__init__()
         assert d_model % h == 0
         # We assume d_v always equals d_k
-        self.hidden_size = d_model*12
+        self.hidden_size = d_model*16
         self.d_k = self.hidden_size // h
         self.h = h
         self.linears = nn.ModuleList([nn.Linear(d_model,self.hidden_size) for _ in range(3)])
