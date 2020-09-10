5e122d86cf40de2ffb43f289ed479c71f8e05cfb
oklens <3294005475@qq.com>
2020-09-10 11:44:21 +0800

    update

--- src_nq/runDr4ABLS.py
+++ src_nq/runDr4ABLS.py
@@ -466,14 +466,14 @@ def main():
                     else:
                         loss.backward()
                     
-#                    torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+                    torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
                     
                     if (step + 1) % args.gradient_accumulation_steps == 0:
 #                        gc.collect() 
 #                        torch.cuda.empty_cache()
+                        optimizer.zero_grad()
                         optimizer.step()
                         scheduler.step()
-                        optimizer.zero_grad()
                         global_step += 1
 
 
