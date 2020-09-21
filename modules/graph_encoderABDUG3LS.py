2ab5c3c6bd8df20a600171d52c7a4ccc9cb4278a
oklen <3294005475@qq.com>
2020-09-21 09:07:13 +0800

    Get Mix Attention

--- modules/graph_encoderABDUG3LS.py
+++ modules/graph_encoderABDUG3LS.py
@@ -535,13 +535,14 @@ class MultiHeadedAttention(nn.Module):
         x = x.transpose(1, 2).contiguous() \
              .view(nbatches, -1, self.h * self.d_k)
         return self.output(x)
+    
 class getMaxScore(nn.Module):
     def __init__(self,d_model,dropout = 0.1):
         super(getMaxScore, self).__init__()
         self.hidden_size = d_model
         self.linears = nn.ModuleList([nn.Linear(d_model,self.hidden_size) for _ in range(2)])
         self.dropout = nn.Dropout(dropout)
-        self.k = 2
+        self.k = 3
     
     def forward(self,query,key):
         query,key = self.linears[0](query),self.linears[1](key)
@@ -550,6 +551,7 @@ class getMaxScore(nn.Module):
         topks = []
         for i in range(self.k):
             MaxInd=torch.argmax(scores)
+            if scores[MaxInd] == -100000: break
             scores[MaxInd] = -100000
             topks.append(key[MaxInd])
         return torch.mean(torch.stack(topks),0)
@@ -757,10 +759,10 @@ class Encoder(nn.Module):
             V21 = hidden_states3[i][qas[i]]
             V22 = hidden_states4[i][qas[i]]
             
-            # V11 = torch.mean(hidden_states3[i][sen_ss[i][:-1,0]],0)
+            V11 = torch.mean(hidden_states3[i][sen_ss[i][:-1,0]],0)
             # V12 = torch.mean(hidden_states4[i][sen_ss[i][:-1,0]],0)
             
-            V11 = self.TopNet[0](V21,hidden_states[i][sen_ss[i][:-1,0]])
+            # V11 = self.TopNet[0](V21,hidden_states[i][sen_ss[i][:-1,0]])
             V12 = self.TopNet[1](V22, hidden_states4[i][sen_ss[i][:-1,0]])
             
             
