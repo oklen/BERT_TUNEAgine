ac578e61b298f90c423dabf73edfd0e0a7554f8b
oklen <3294005475@qq.com>
2020-09-30 09:49:24 +0800

    Large_SIM

--- modules/graph_encoderABDUG5LS.py
+++ modules/graph_encoderABDUG5LS.py
@@ -292,11 +292,11 @@ class Encoder(nn.Module):
         self.conv2 = torch.nn.ModuleList()
         for i in range(4):
             self.conv2.append(
-                    DNAConv(config.hidden_size,self.att_heads,1,0.5))
+                    DNAConv(config.hidden_size,self.att_heads,1,0.4))
         self.conv3 = torch.nn.ModuleList()
         for i in range(4):
             self.conv3.append(
-                DNAConv(config.hidden_size,self.att_heads,1,0,0.5))
+                DNAConv(config.hidden_size,self.att_heads,1,0,0.4))
         # self.conv = GraphConv(config.hidden_size, config.hidden_size,'max')
             
         self.lineSub = torch.nn.Linear(config.hidden_size*3,config.hidden_size)
