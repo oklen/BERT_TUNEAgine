3985d644409c9c1ab78ec8358bb279bed4087180
oklen <3294005475@qq.com>
2020-10-01 20:18:40 +0800

    Large_SIM

--- modules/graph_encoderABDUG52LS.py
+++ modules/graph_encoderABDUG52LS.py
@@ -172,6 +172,9 @@ class LayerNorm(nn.Module):
 def attention(query, key, value, mask=None, dropout=None):
     "Compute 'Scaled Dot Product Attention'"
     d_k = query.size(-1)
+    query = gelu(query)
+    key = gelu(query)
+    
     scores = torch.matmul(query, key.transpose(-2, -1)) \
              / math.sqrt(d_k)
     if mask is not None:
--- src_nq/modelDRAB2LS.py
+++ src_nq/modelDRAB2LS.py
@@ -109,14 +109,15 @@ class NqModel(nn.Module):
                     edges_src, edges_tgt, edges_type, edges_pos,))
                 else:
                     graph_output = self.encoder(sequence_output, st_mask, edges_src, edges_tgt, edges_type, edges_pos, all_sen,output_all_encoded_layers=False)
-                    x = self.dropout(graph_output)
+                    # x = self.dropout(graph_output)
+                    x = graph_output
                 
 #                    x = self.dropout(sequence_output[:,0])
 #                    print(x)
 #                    x = self.dropout(graph_output)
                     # tok_logits.append(self.tok_outputs(x).squeeze(-1))
                     # x = self.dropout(graph_output)
-                    tok_logits.append(self.tok_outputs(self.dropout(torch.tanh(self.tok_dense(x)))).squeeze(-1))
+                    tok_logits.append(self.tok_outputs(self.dropout(torch.relu(self.tok_dense(x)))).squeeze(-1))
 
             else:
                 input_ids = input_ids.to('cuda:0')
