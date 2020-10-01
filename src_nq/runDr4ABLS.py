ac20772b44a83674d28a083247e20aa17eff7052
oklen <3294005475@qq.com>
2020-10-01 16:14:41 +0800

    Large_SIM

--- src_nq/runDr4ABLS.py
+++ src_nq/runDr4ABLS.py
@@ -417,13 +417,13 @@ def main():
         optimizer = AdamW(optimizer_grouped_parameters, lr=args.learning_rate, eps=args.adam_epsilon)
 #        optimizer = SGD(optimizer_grouped_parameters, lr=args.learning_rate,momentum=0.9)
         
-        # scheduler = WarmupLinearSchedule(optimizer,
+        scheduler = WarmupLinearSchedule(optimizer,
+                                      warmup_steps=int(args.warmup_proportion * num_train_optimization_steps)
+                                      if args.warmup_proportion > 0 else args.warmup_steps,
+                                      t_total=num_train_optimization_steps)
+        # scheduler = WarmupConstantSchedule(optimizer,
         #                              warmup_steps=int(args.warmup_proportion * num_train_optimization_steps)
-        #                              if args.warmup_proportion > 0 else args.warmup_steps,
-        #                              t_total=num_train_optimization_steps)
-        scheduler = WarmupConstantSchedule(optimizer,
-                                     warmup_steps=int(args.warmup_proportion * num_train_optimization_steps)
-                                     if args.warmup_proportion > 0 else args.warmup_steps)
+        #                              if args.warmup_proportion > 0 else args.warmup_steps)
 
         
 
