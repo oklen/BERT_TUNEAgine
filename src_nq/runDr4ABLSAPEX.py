c0a297ea86cf72fbd59343a909355a2336c0ba8e
oklen <3294005475@qq.com>
2020-10-02 16:13:21 +0800

    FreeLB_ADDed

--- src_nq/runDr4ABLSAPEX.py
+++ src_nq/runDr4ABLSAPEX.py
@@ -431,7 +431,7 @@ def main():
     global_step = 0
     last_acc = 89.5
     albert_toker = AlbertTokenizer.from_pretrained('albert-base-v2')
-    model, optimizer = amp.initialize(model, optimizer, opt_level="O2")
+    model, optimizer = amp.initialize(model, optimizer, opt_level="O1")
     # torch.autograd.set_detect_anomaly(True)
     
     if args.do_train:
