6a236fb01daa9531b0755ccab1691a69228a7cea
oklen <3294005475@qq.com>
2020-10-02 16:09:47 +0800

    FreeLB_ADDed

--- src_nq/modelDRAB3LS.py
+++ src_nq/modelDRAB3LS.py
@@ -106,7 +106,7 @@ class NqModel(nn.Module):
                 if input_embs==None:
                     sequence_output,_ = self.bert(input_ids,  attention_mask,token_type_ids)
                 else:
-                    sequence_output,_ = self.bert(input_ids,  attention_mask,token_type_ids,input_embs=inputs_embs.long())
+                    sequence_output,_ = self.bert(input_ids,  attention_mask,token_type_ids,inputs_embs=input_embs.long())
                 if getattr(self.bert_config, "gradient_checkpointingNot", False):
                     def create_custom_forward(module):
                         def custom_forward(*inputs,output_all_encoded_layers=False):
