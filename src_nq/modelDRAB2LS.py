28af1414fe7928ff38a5e658804cd82a115c2814
oklen <3294005475@qq.com>
2020-10-03 18:44:17 +0800

    FreeLB_ADDed

--- src_nq/modelDRAB2LS.py
+++ src_nq/modelDRAB2LS.py
@@ -37,7 +37,7 @@ class NqModel(nn.Module):
 
         #print(my_config,bert_config)
 #        self.tok_dense = nn.Linear(my_config.hidden_size, my_config.hidden_size)
-        self.tok_dense = nn.Linear(my_config.hidden_size*3, my_config.hidden_size*3)
+        self.tok_dense = nn.Linear(my_config.hidden_size*2, my_config.hidden_size*2)
 
 #        self.tok_dense2 = nn.Linear(my_config.hidden_size, my_config.hidden_size)
 #        self.para_dense = nn.Linear(self.config.hidden_size, self.config.hidden_size)
@@ -45,7 +45,7 @@ class NqModel(nn.Module):
         
         self.dropout = nn.Dropout(my_config.hidden_dropout_prob)
 
-        self.tok_outputs = nn.Linear(my_config.hidden_size*3, 1) # tune to avoid fell into bad places
+        self.tok_outputs = nn.Linear(my_config.hidden_size*2, 1) # tune to avoid fell into bad places
         
 #        self.tok_outputs2 = nn.Linear(my_config.hidden_size, 1)
 #        config.max_token_len, config.max_token_relative
