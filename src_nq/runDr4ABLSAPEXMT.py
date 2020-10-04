7a2f4740e85eaeea31e2782c10a317c967f8f730
oklen <3294005475@qq.com>
2020-10-04 09:28:40 +0800

    FreeLB_ADDed

--- src_nq/runDr4ABLSAPEXMT.py
+++ src_nq/runDr4ABLSAPEXMT.py
@@ -550,7 +550,7 @@ def main():
                 model.train()
                 
                 logging.info("Step Pre Epoch: {} ".format(int(feature_cnt)//args.gradient_accumulation_steps))
-                for step, _ in enumerate(range(int(feature_cnt)//args.gradient_accumulation_steps)):
+                for step, _ in enumerate(range(int(feature_cnt))):
                     # if not Err_test:
                     #     WrOut = ""
                     #     for i in albert_toker.convert_ids_to_tokens(batch.input_ids[0][0]):
