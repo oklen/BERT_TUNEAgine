2b963c99eca17404c7f462c2ea2ac3616d1bfdb0
oklen <3294005475@qq.com>
2020-10-06 20:44:19 +0800

    FreeLB_ADDed

--- modules/graph_encoderABDUG4LS3.py
+++ modules/graph_encoderABDUG4LS3.py
@@ -787,12 +787,15 @@ class Encoder(nn.Module):
                 if j==0:
                     hq2q1 = hq2q1.squeeze(0)
                     hq1q2 = hq1q2.squeeze(0)
-                    hidden_states2[i][1:(all_sen_now[-1][0]-1)] = self.ctoq(hq1q2,hq1q2,hq1q2)
+                    # hidden_states2[i][1:(all_sen_now[-1][0]-1)] = self.ctoq(hq1q2,hq1q2,hq1q2)
+                    hidden_states2[i][1:(all_sen_now[-1][0]-1)] = hq1q2
+
                     hidden_states2[i][all_sen_now[-1][0]:all_sen_now[-1][1]] = hq2q1
                 else:
                     hq2q12 = hq2q12.squeeze(0)
                     hq1q22 = hq1q22.squeeze(0)
-                    hidden_states22[i][1:(all_sen_now[-1][0]-1)] = self.ctoq(hq1q22,hq1q22,hq1q22)
+                    # hidden_states22[i][1:(all_sen_now[-1][0]-1)] = self.ctoq(hq1q22,hq1q22,hq1q22)
+                    hidden_states22[i][1:(all_sen_now[-1][0]-1)] = hq1q22
                     hidden_states22[i][all_sen_now[-1][0]:all_sen_now[-1][1]] = hq2q12
 #            
             
