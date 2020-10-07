8cd9d321772e7e921c527dbe2c83b0d59cd700c8
oklen <3294005475@qq.com>
2020-10-07 19:03:06 +0800

    FreeLB_ADDed

--- src_nq/runDr5ABLS.py
+++ src_nq/runDr5ABLS.py
@@ -532,7 +532,7 @@ def main():
             logging.info("ACC:{}% LOSS:{}".format(model.ACC/model.ALL*100,ttr_loss/tgobal_step))
             model.zero_grad()
             optimizer.zero_grad()
-            model.encoder.TopNet[0].improveit() #Use decreased K
+            model.encoder.TopNet[0].improveit() #Use scheludar K
             logging.info("Next K use:{}".format(model.encoder.TopNet[0].k))
             if model.ACC/model.ALL*100>last_acc:
                 logging.info("Save Model")
