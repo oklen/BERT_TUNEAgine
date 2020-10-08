26511a6ccf8e57b9423dd3a53f22b769ccc67925
oklen <3294005475@qq.com>
2020-10-08 09:23:41 +0800

    FreeLB_ADDed

--- src_nq/modelDRAB3LS2.py
+++ src_nq/modelDRAB3LS2.py
@@ -135,7 +135,7 @@ class NqModel(nn.Module):
 #                    x = self.dropout(graph_output)
                     Output = graph_output.view(graph_output.size(0),self.bert_config.max_position_embeddings,-1)
                     # print("shape:",Output.shape,_.shape)
-                    output_scores = torch.bmm(Output,sequence_output[:,0].transpose(-1,-2))
+                    output_scores = torch.bmm(Output,_.unsequeeze(1).transpose(-1,-2))
                     tok_logits.append(self.tok_outputs((output_scores*Output).view(graph_output)).squeeze(-1))
                     # tok_logits.append(self.tok_outputs(graph_output).squeeze(-1))
                     # tok_logits.append(graph_output.squeeze(-1))
