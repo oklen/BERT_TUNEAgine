06ea86b724fcac580ef819df1c5b3e9629e6e26a
oklen <3294005475@qq.com>
2020-10-09 19:43:38 +0800

    CLS_Attention

--- src_nq/modelDRAB2LSM.py
+++ src_nq/modelDRAB2LSM.py
@@ -113,7 +113,7 @@ class NqModel(nn.Module):
     #                    x = self.dropout(graph_output)
                         # tok_logits.append(self.tok_outputs(x).squeeze(-1))
                         # x = self.dropout(graph_output)
-                        tok_logits.append(self.tok_outputs(self.dropout(torch.tanh(self.tok_dense(x)))).squeeze(-1))
+                        tok_logits.append(self.tok_outputs(x).squeeze(-1))
     
                 else:
                     input_ids = input_ids.to('cuda:0')
