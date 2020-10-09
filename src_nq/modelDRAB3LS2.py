b50be3b920559ead28220556d4ad672c472edef7
oklen <3294005475@qq.com>
2020-10-09 15:23:43 +0800

    CLS_Attention

--- src_nq/modelDRAB3LS2.py
+++ src_nq/modelDRAB3LS2.py
@@ -5,9 +5,19 @@ import torch.nn as nn
 
 from modules.graph_encoderABDUG4LS4V import NodeType, NodePosition, EdgeType, Encoder,GraphEncoder
 from transformers import AutoTokenizer, AutoModelWithLMHead,AutoModel,AlbertModel,AlbertConfig,RobertaModel,RobertaConfig
+import math
 #  elgeish/cs224n-squad2.0-albert-large-v2
 #  albert-large-v2
 #
+
+def gelu(x):
+    """Implementation of the gelu activation function.
+        For information: OpenAI GPT's gelu is slightly different (and gives slightly different results):
+        0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
+        Also see https://arxiv.org/abs/1606.08415
+    """
+    return x * 0.5 * (1.0 + torch.erf(x / math.sqrt(2.0)))
+
 class BertOutput(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -81,7 +91,11 @@ class NqModel(nn.Module):
 #        self.encoder2 = Encoder(my_config)
         
         self.my_config = my_config
-        self.output_act = torch.nn.functional.gelu
+        if torch.__version__ == '1.1.0':
+            self.output_act = gelu
+        else:
+            self.output_act = torch.nn.functional.gelu
+        # self.output_act = torch.nn.functional.gelu
         
 #        self.my_mask = 
 
